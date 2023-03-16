3381d2a053e1984dc659fd270e19c5e9f0a6eb82
HP <HP@DESKTOP-4Q8V1JF>
2023-03-17 00:22:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+           
+                Console.WriteLine("Enter your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+                
+                if (age > 18)
+                {
+                    Console.WriteLine("You can drive ");
+                }
+                else
+                {
+                    Console.WriteLine("You cant drive ");
+                }
+            }
+        }
+
+    }
+}
